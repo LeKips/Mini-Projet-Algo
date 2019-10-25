df2b0b038f13adafc14ce7cc8bf3c1cb99e87c79
LeKips <56925949+LeKips@users.noreply.github.com>
2019-10-25 23:35:27 +0200

    Add files via upload

---
+++ dico.py
@@ -0,0 +1,103 @@
+villes = {
+    "Bordeaux": {
+        "Poitiers": 250,
+        "Pau": 217,
+        "Lille": 801,
+        "Orthez": 191,
+        "Royan": 131,
+        "Bayonne": 184,
+        "Mulhouse": 832,
+        "Toulouse": 245,
+        "Paris": 588
+    },
+
+    "Bayonne": {
+        "Lille": 986 ,
+        "Mulhouse" 1013,
+        "Orthez": 73.5,
+        "Pau": 112,
+        "Paris" 771,
+        "Royan": 319,
+        "Toulouse": 300,
+    },
+
+    "Lille": {
+        "Pau": 1010,
+        "Orthez": 1028,
+        "Royan": 727,
+        "Bayonne": 985,
+        "Mulhouse": 595,
+        "Toulouse": 894,
+        "Paris": 225,
+    },
+
+    "Mulhouse": {
+        "Pau": 1039,
+        "Lille": 596,
+        "Orthez": 1057,
+        "Royan": 915,
+        "Bayonne": 1014,
+        "Toulouse": 836,
+        "Paris": 538,
+    },
+
+    "Orthez": {
+        "Pau": 46.5,
+        "Lille": 985,
+        "Royan": 363,
+        "Bayonne": 73.7,
+        "Mulhouse": 1057,
+        "Toulouse": 234,
+        "Paris": 770,
+    },
+
+    "Pau": {
+        "Lille": 1011,
+        "Orthez": 47.1,
+        "Royan": 344,
+        "Bayonne": 112,
+        "Mulhouse": 1026,
+        "Toulouse": 199,
+        "Paris": 796,
+    },
+
+    "Paris": {
+        "Pau": 796,
+        "Lille": 225,
+        "Orthez": 812,
+        "Royan": 511,
+        "Bayonne": 769,
+        "Mulhouse": 537,
+        "Toulouse": 678,
+    },
+
+    "Poitiers": {
+        "Pau": 461,
+        "Lille": 561,
+        "Orthez": 479,
+        "Royan": 178,
+        "Bayonne": 435,
+        "Mulhouse": 748,
+        "Toulouse": 489,
+        "Paris": 345,
+    },
+    "Royan": {
+        "Pau": 334,
+        "Lille": 729,
+        "Orthez": 352,
+        "Bayonne": 309,
+        "Mulhouse": 916,
+        "Toulouse": 362,
+        "Paris": 513,
+    },
+
+    "Toulouse": {
+        "Pau": 197,
+        "Lille": 896,
+        "Orthez": 231,
+        "Royan": 371,
+        "Bayonne": 296,
+        "Mulhouse": 835,
+        "Paris": 680,
+    }
+}
