b9569fde79725f74243b2c40935d94cb63c6c306
LeKips <56925949+LeKips@users.noreply.github.com>
2019-10-25 23:39:26 +0200

    Add files via upload

--- LaCarte.py
+++ LaCarte.py
@@ -1,1 +1,13 @@
 from dico import villes
+def depart():
+
+    listeVilles = []
+    choixVille = None
+    compteur = 1
+    tempsTotal = 0 
+    trajetTotal = 0
+
+print("Choisissez votre ville de départ et votre arrivé, puis appuyez sur ENTRER")
+
+    while choixVille != '' :
+        print('Ville{} :
